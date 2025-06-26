43a5e2fc61d579365e03a05bf4df4a0390d1d49e
Philip K. Gisslow <ripxorip@gmail.com>
2025-06-26 18:41:54 +0200

    feat: Adds pipewire routing, next up Rock N Roll

--- scripts/connect.sh
+++ scripts/connect.sh
@@ -1,8 +1,10 @@
 #!/usr/bin/env bash
 # Sample connect.sh for Ghost Rig
 
-echo "[connect.sh] This is a sample connection script."
-echo "[connect.sh] Here you would use pw-link or similar to wire up your PipeWire nodes."
-# Example (uncomment and adapt):
-# pw-link 'PWAR:output-left' 'system:playback_1'
-# pw-link 'PWAR:output-right' 'system:playback_2'
+echo "[connect.sh] Connecting ports..."
+echo "[connect.sh] Waiting for the PipeWire nodes to settle..."
+sleep 2
+pw-link alsa_input.usb-Focusrite_Scarlett_6i6_USB_00003367-00.analog-surround-21:capture_FR pw-ghost-rec:input
+pw-link pw-ghost-rec:output-right pwar:input
+pw-link pwar:output-left alsa_output.usb-Focusrite_Scarlett_6i6_USB_00003367-00.analog-surround-21:playback_FL
+pw-link pwar:output-right alsa_output.usb-Focusrite_Scarlett_6i6_USB_00003367-00.analog-surround-21:playback_FR
