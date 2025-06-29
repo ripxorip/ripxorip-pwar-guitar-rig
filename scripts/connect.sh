e5d055422ebd551939399ac528f43d5238054f6e
Philip K. Gisslow <ripxorip@gmail.com>
2025-06-29 10:50:11 +0200

    fix: Connection timings

--- scripts/connect.sh
+++ scripts/connect.sh
@@ -3,8 +3,10 @@
 
 echo "[connect.sh] Connecting ports..."
 echo "[connect.sh] Waiting for the PipeWire nodes to settle..."
-sleep 2
 pw-link alsa_input.usb-Focusrite_Scarlett_6i6_USB_00003367-00.analog-surround-21:capture_FR pw-ghost-rec:input
+sleep 1
 pw-link pw-ghost-rec:output-right pwar:input
+sleep 1
 pw-link pwar:output-left alsa_output.usb-Focusrite_Scarlett_6i6_USB_00003367-00.analog-surround-21:playback_FL
 pw-link pwar:output-right alsa_output.usb-Focusrite_Scarlett_6i6_USB_00003367-00.analog-surround-21:playback_FR
+echo "[connect.sh] Connections established."
