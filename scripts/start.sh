54bd356d09efebed5df522ee3d6a13d7248b23fc
Philip K. Gisslow <ripxorip@gmail.com>
2025-06-29 10:56:35 +0200

    fix: Rapid printing

--- scripts/start.sh
+++ scripts/start.sh
@@ -14,17 +14,17 @@ echo "[Ghost Rig] Setting latency to 128/48000..."
 export PIPEWIRE_LATENCY=128/48000
 
 echo "[1] Starting PWAR..."
-( nix run .#pwar | sed 's/^/[PWAR]: /' ) &
+( stdbuf -oL nix run .#pwar | sed 's/^/[PWAR]: /' ) &
 PWAR_PID=$!
 PWAR_PGID=$PWAR_PID
 
 echo "[2] Starting pw-ghost-rec..."
-( nix run .#pw-ghost-rec | sed 's/^/[REC]: /' ) &
+( stdbuf -oL nix run .#pw-ghost-rec | sed 's/^/[REC]: /' ) &
 REC_PID=$!
 REC_PGID=$REC_PID
 
 echo "[3] Starting midi_udp_streamer..."
-( nix run .#midi-udp-streamer | sed 's/^/[MIDI]: /' ) &
+( stdbuf -oL nix run .#midi-udp-streamer | sed 's/^/[MIDI]: /' ) &
 MIDI_PID=$!
 MIDI_PGID=$MIDI_PID
 
