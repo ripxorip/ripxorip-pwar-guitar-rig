94204ec329b3a5172f19176699fb3f59f64226a2
Philip K. Gisslow <ripxorip@gmail.com>
2025-06-29 21:47:22 +0200

    fix: Stdout for errors instead

--- scripts/start.sh
+++ scripts/start.sh
@@ -14,17 +14,23 @@ echo "[Ghost Rig] Setting latency to 128/48000..."
 export PIPEWIRE_LATENCY=128/48000
 
 echo "[1] Starting PWAR..."
-( stdbuf -oL nix run .#pwar | sed 's/^/[PWAR]: /' ) &
+# Define color codes using $'...' for proper escape interpretation
+GREEN=$'\033[0;32m'
+BLUE=$'\033[0;34m'
+ORANGE=$'\033[38;5;208m'
+NC=$'\033[0m' # No Color
+
+( stdbuf -oL nix run .#pwar | sed "s/^/${GREEN}[PWAR]: ${NC}/" ) &
 PWAR_PID=$!
 PWAR_PGID=$PWAR_PID
 
 echo "[2] Starting pw-ghost-rec..."
-( stdbuf -oL nix run .#pw-ghost-rec | sed 's/^/[REC]: /' ) &
+( stdbuf -oL nix run .#pw-ghost-rec | sed "s/^/${BLUE}[REC]: ${NC}/" ) &
 REC_PID=$!
 REC_PGID=$REC_PID
 
 echo "[3] Starting midi_udp_streamer..."
-( stdbuf -oL nix run .#midi-udp-streamer | sed 's/^/[MIDI]: /' ) &
+( stdbuf -oL nix run .#midi-udp-streamer | sed "s/^/${ORANGE}[MIDI]: ${NC}/" ) &
 MIDI_PID=$!
 MIDI_PGID=$MIDI_PID
 
